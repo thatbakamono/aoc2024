83aee8cc9c5b02d9b07bc577f2d014f289ee5b94
Dorian Kostecki <dorian.kostecki@gmail.com>
2024-12-09 12:14:56 +0100

    Add solutions for day 7

---
+++ day7-part1/src/Main.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import Control.Monad (replicateM)
+import Data.Maybe (catMaybes)
+import Text.Read (readMaybe)
+
+parseLine :: String -> Maybe (Int, [Int])
+parseLine line =
+  case span (/= ':') line of
+    (keyStr, ':' : rest) -> do
+      key <- readMaybe keyStr :: Maybe Int
+      let valueStrs = words rest
+      values <- mapM (readMaybe :: String -> Maybe Int) valueStrs
+      return (key, values)
+    _ -> Nothing
+
+parseInput :: String -> [(Int, [Int])]
+parseInput input = catMaybes $ map parseLine (lines input)
+
+generateCombinations :: [Int] -> [[Int -> Int -> Int]]
+generateCombinations nums = replicateM (length nums - 1) [(+), (*)]
+
+applyOperations :: [Int] -> [Int -> Int -> Int] -> Int
+applyOperations (x : xs) ops = foldl (\acc (op, n) -> op acc n) x (zip ops xs)
+applyOperations _ _ = error "List of numbers must not be empty"
+
+generateResults :: [Int] -> [Int]
+generateResults nums =
+  [applyOperations nums ops | ops <- generateCombinations nums]
+
+isValid :: (Int, [Int]) -> Bool
+isValid (testValue, numbers) = testValue `elem` generateResults numbers
+
+main :: IO ()
+main = do
+  content <- readFile "input.txt"
+
+  let input = parseInput content
+  let validInput = filter isValid input
+  let result = sum (map fst validInput)
+
+  print result
